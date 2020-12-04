4de64bbb2d394581853779c381f2a8447e3caa87
Sambo3975 <kni16008@byui.edu>
2020-12-04 10:46:57 -0800

    Day 4: Some improvements

--- day_4_passport_processing/solution.py
+++ day_4_passport_processing/solution.py
@@ -14,90 +14,46 @@ def read_input(file_name):
     return passports
 
 
-required_fields = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid'
-]
-
-
-def check_bounded(value, mini, maxi):
-    return mini <= int(value) <= maxi
-
-
 def check_height(value):
-    number = value[:-2]
+    number = int(value[:-2])
     unit = value[-2:]
-    return unit == 'cm' and check_bounded(number, 150, 193) \
-        or unit == 'in' and check_bounded(number, 59, 76)
-
-
-def check_hair_color(value):
-    return re.match(r'^#([0-9]|[a-f]){6}$', value)
+    return unit == 'cm' and 150 <= number <= 193 \
+        or unit == 'in' and 59 <= number <= 76
 
 
 valid_eye_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+field_checks = {
+    'byr': lambda value: 1920 <= int(value) <= 2002,
+    'iyr': lambda value: 2010 <= int(value) <= 2020,
+    'eyr': lambda value: 2020 <= int(value) <= 2030,
+    'hgt': check_height,
+    'hcl': lambda value: re.match(r'^#([0-9]|[a-f]){6}$', value),
+    'ecl': lambda value: value in valid_eye_colors,
+    'pid': lambda value: re.match(r'^[0-9]{9}$', value)
+}
 
 
-def check_eye_color(value):
-    return value in valid_eye_colors
-
-
-def check_passport_id(value):
-    return re.match(r'^[0-9]{9}$', value)
-
-
-def validate_passports(passports):
-    valid_ct = 0
-    for passport in passports:
-        invalid = False
-        # Check for each required field
-        for field in required_fields:
-            if field not in passport:
-                invalid = True
-        if not invalid:
-            valid_ct += 1
-    return valid_ct
-
-
-def validate_passports_properly(passports):
-    valid_ct = 0
+def validate_passports(passports, check_fields=True):
+    valid_count = 0
     for x in passports:
-        print(x)
         invalid = False
         # Check for each required field
-        for field in required_fields:
-            if field not in x:
+        for field in field_checks:
+            if not (field in x and (not check_fields or field_checks[field](x[field]))):
                 invalid = True
                 break
-        print('passed first check')
-
-        # Check fields for valid values
-        if not invalid \
-                and check_bounded(x['byr'], 1920, 2002) \
-                and check_bounded(x['iyr'], 2010, 2020) \
-                and check_bounded(x['eyr'], 2020, 2030) \
-                and check_height(x['hgt']) \
-                and check_hair_color(x['hcl']) \
-                and check_eye_color(x['ecl']) \
-                and check_passport_id(x['pid']):
-            print('passed second check')
-            valid_ct += 1
-
-    return valid_ct
+        if not invalid:
+            valid_count += 1
+    return valid_count
 
 
 if __name__ == '__main__':
     passes = read_input('input.txt')
 
     # Part 1
-    valid_ct = validate_passports(passes)
-    print('Valid passports: {}'.format(valid_ct))
+    valid_ct = validate_passports(passes, False)
+    print('Passports with all required fields: {}'.format(valid_ct))
 
     # Part 2
-    valid_ct = validate_passports_properly(passes)
-    print('Validly Valid passports: {}'.format(valid_ct))
+    valid_ct = validate_passports(passes)
+    print('Valid passports: {}'.format(valid_ct))
